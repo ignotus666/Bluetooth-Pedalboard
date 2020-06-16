ebb9f173b2f5a3e6aa7a61e9a6ad12c66cae8cb9
ignotus <darylo1@hotmail.com>
2020-06-16 14:26:42 +0200

    Update active_preset_bank.ino

--- Pedalboard_0.98_5_banks/active_preset_bank.ino
+++ Pedalboard_0.98_5_banks/active_preset_bank.ino
@@ -5,7 +5,7 @@ void banks()
   b = bankNumber;
   for (int k = 0; k < 6; k++)
   {
-    if (keyPressed[k])              //Check which button has been pressed. Don't change if wah pressed (for calibration).
+    if (keyPressed[k])                                      //Check which button has been pressed.
     {
       MIDI.sendProgramChange(k + (b * 6), 1);               //Send a MIDI Program Change message (value, channel). Multiply bank No. by 6 (no. of preset footswitches) and add to k to get note.
       midiLedOn();                                          //Flash MIDI LED.
